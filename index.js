67e05bd96ff3d567df1f280d8a3420ce1677210f
Zacharia Ndinguri <zacthuku@gmail.com>
2025-02-27 08:24:09 +0300

    complete-lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop("Ralph");
+}
+function destructivelyRemoveFirstCat(Bob){
+    cats.shift("Bob");
+}
+function appendCat(Broom) {
+    return ["Milo", "Otis", "Garfield", "Broom"]; 
+}
+function prependCat(Arnold){
+    return["Arnold", "Milo", "Otis", "Garfield"];
+}
+function removeLastCat() {
+    return cats.slice(0, -1);  
+}
+function removeFirstCat() {
+    return cats.slice(1);  
+}
